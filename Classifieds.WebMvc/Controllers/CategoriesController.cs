1838657f55ba6b4793fec013b6214644231dd887
Nyein Chan Oo <chanoo.dev@gmail.com>
2023-04-14 10:42:05 +0800

    Init

---
+++ Classifieds.WebMvc/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Classifieds.Data;
+using Classifieds.Data.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Classifieds.WebMvc.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public CategoriesController(ApplicationDbContext context) {
+            _context=context;
+        }
+        public IActionResult Index()
+        {
+            List<Category> categories=_context.Categories.ToList();
+            return View(categories);
+        }
+    }
+}
---
+++ Classifieds.WebMvc/Program.cs
@@ -0,0 +1,40 @@
+using Classifieds.Data;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.EntityFrameworkCore;
+using System.Net;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews(q=>q.Filters.Add(new AuthorizeFilter()));
+builder.Services.AddDbContext<ApplicationDbContext>(
+    opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("DBConnection"))
+    .EnableSensitiveDataLogging()
+    .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking));
+
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(x => x.LoginPath = "/account/login");
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
